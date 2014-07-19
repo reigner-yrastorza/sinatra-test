0a04350f8e5d81162e4a7b9ae6193f9f4b933bde
Reigner S. Yrastorza <reigner@yrastorza.us>
2014-07-19 09:45:38 -0700

    test
    test

---
+++ test.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'sinatra'
+
+configure do
+  file = File.new("requests.log", 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
+end
+
+get '/' do
+  "The current time is #{Time.now.strftime('%H:%M')}"
+end
